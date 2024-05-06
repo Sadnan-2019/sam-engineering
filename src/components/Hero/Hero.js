e1eda81a22589970d5332b936f7f3723e86461b0
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-05-06 22:51:53 +0600

    partner

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
+import banner1 from "../../assets/banner1.jpg"
+import banner2 from "../../assets/banner2.jpg"
+import banner3 from "../../assets/banner3.jpg"
+import banner4 from "../../assets/banner4.jpg"
 const Hero = () => {
      return (
           <div>
@@ -15,15 +18,12 @@ const Hero = () => {
         >
           <div class="carousel-item active">
             <div class="carousel-container">
-              <h2 class="animate__animated animate__fadeInDown">
+              <h2 class=" animate__animated animate__fadeInDown">
                 Welcome to <span>Anyar</span>
               </h2>
-              <p class="animate__animated animate__fadeInUp">
-                Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et
-                est quaerat sequi nihil ut aliquam. Occaecati alias dolorem
-                mollitia ut. Similique ea voluptatem. Esse doloremque accusamus
-                repellendus deleniti vel. Minus et tempore modi architecto.
-              </p>
+              <img src={banner1}  alt='' class="rounded animate__animated animate__fadeInUp w-50"/>
+               
+              
               <a
                 href="#about"
                 class="btn-get-started animate__animated animate__fadeInUp scrollto"
@@ -38,12 +38,7 @@ const Hero = () => {
               <h2 class="animate__animated animate__fadeInDown">
                 Lorem Ipsum Dolor
               </h2>
-              <p class="animate__animated animate__fadeInUp">
-                Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et
-                est quaerat sequi nihil ut aliquam. Occaecati alias dolorem
-                mollitia ut. Similique ea voluptatem. Esse doloremque accusamus
-                repellendus deleniti vel. Minus et tempore modi architecto.
-              </p>
+              <img src={banner2}  alt='' class="animate__animated animate__fadeInUp w-50 rounded"/>
               <a
                 href="#about"
                 class="btn-get-started animate__animated animate__fadeInUp scrollto"
@@ -58,12 +53,21 @@ const Hero = () => {
               <h2 class="animate__animated animate__fadeInDown">
                 Sequi ea ut et est quaerat
               </h2>
-              <p class="animate__animated animate__fadeInUp">
-                Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et
-                est quaerat sequi nihil ut aliquam. Occaecati alias dolorem
-                mollitia ut. Similique ea voluptatem. Esse doloremque accusamus
-                repellendus deleniti vel. Minus et tempore modi architecto.
-              </p>
+              <img src={banner3}  alt='' class="animate__animated animate__fadeInUp w-50 rounded"/>
+              <a
+                href="#about"
+                class="btn-get-started animate__animated animate__fadeInUp scrollto"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="carousel-container">
+              <h2 class="animate__animated animate__fadeInDown">
+                Sequi ea ut et est quaerat
+              </h2>
+              <img src={banner4}  alt='' class="animate__animated animate__fadeInUp   w-50 rounded"/>
               <a
                 href="#about"
                 class="btn-get-started animate__animated animate__fadeInUp scrollto"
