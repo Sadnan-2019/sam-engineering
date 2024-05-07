6ab32de2c9b630c1b78a12dfe97467c5daf53d2e
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-05-07 23:12:48 +0600

    partner

--- src/components/Product/ProductDetails.js
+++ src/components/Product/ProductDetails.js
@@ -1,34 +1,46 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-
+import React from "react";
+import { useNavigate } from "react-router";
+import "./ProductDetails.css";
 const ProductDetails = (props) => {
-     const {id,name,img } = props.product;
+  const { id, name, img } = props.product;
 
-     const navigate= useNavigate()
+  const navigate = useNavigate();
 
-     const handleServiceDetails=(id)=>{
-       navigate(`/product-details/${id}`)
+  const handleServiceDetails = (id) => {
+    navigate(`/product-details/${id}`);
+  };
+  return (
+    <div className=" col-md-6 col-lg-4 g-4">
+      {/* <div class="card">
+        <img src={img} className="   " alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{name}</h5>
+         
 
+          <button
+            className="btn btn-primary buy-now-button"
+            onClick={() => handleServiceDetails(id)}
+          >
+            Service Details
+          </button>
+        </div>
+      </div> */}
+      <div className="card-wrapper">
+    <div className="card text-light">
+        <img src={img} className="card-img-top card-img-custom" alt="..." />
+        <div className="card-body bg-dark text-light">
+            <h5 className="card-title">{name}</h5>
+            {/* <p className="card-text">{description}</p> */}
+        </div>
+        <div className="card-footer">
+            {/* <small className="text-muted">Last updated 3 mins ago</small> */}
+            <button className='btn btn-primary' onClick={() => handleServiceDetails(id)}>Service Details</button>
+        </div>
+    </div>
+</div>
 
-     }
-     return (
-          <div className=' col-md-6 col-lg-4 g-4'>
-             
-                    <div className="card    text-light">
-                      <img src={img} className="card-img-top img-fluid p-4" alt="..."/>
-                      <div className="card-body bg-dark text-light">
-                        <h5 className="card-title">{name}</h5>
-               
-                        {/* <p className="card-text">{description}</p> */}
-                      </div>
-                      <div className="card-footer">
-                        {/* <small className="text-muted">Last updated 3 mins ago</small> */}
-                        <button className='btn btn-primary' onClick={()=>handleServiceDetails(id)}>Service Details</button>
-                      </div>
-                    </div>
-                   
-          </div>
-     );
+    </div>
+  );
 };
 
 export default ProductDetails;
