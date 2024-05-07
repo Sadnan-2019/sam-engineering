f4ffce8f0f2626fcad57dc04430ced7d19c55e81
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-05-08 00:58:38 +0600

    partner

--- src/components/Product/AllProductDetails.js
+++ src/components/Product/AllProductDetails.js
@@ -22,18 +22,19 @@ const AllProductDetails = (props) => {
           
                <div className=' col-md-6 col-lg-4 g-4 mt-5  ' id="product" >
              
-             <div className="card    text-light mt-5   ">
-               <img src={img} className="card-img-top img-fluid p-4" alt="..."/>
-               <div className="card-body bg-dark text-light">
-                 <h5 className="card-title">{name}</h5>
-                 {/* <p>Price:${price}</p> */}
-                 {/* <p className="card-text">{description}</p> */}
-               </div>
-               <div className="card-footer">
-                 {/* <small className="text-muted">Last updated 3 mins ago</small> */}
-                 <button className='btn btn-primary' onClick={()=>handleServiceDetails(id)}>Service Details</button>
-               </div>
-             </div>
+             <div className="card-wrapper">
+    <div className="card text-light">
+        <img src={img} className="card-img-top card-img-custom" alt="..." />
+        <div className="card-body bg-dark text-light">
+            <h5 className="card-title">{name}</h5>
+            {/* <p className="card-text">{description}</p> */}
+        </div>
+        <div className="card-footer">
+            {/* <small className="text-muted">Last updated 3 mins ago</small> */}
+            <button className='btn btn-primary' onClick={() => handleServiceDetails(id)}>Service Details</button>
+        </div>
+    </div>
+</div>
             
    </div>
            
--- src/components/Product/MoreProductDetails.js
+++ src/components/Product/MoreProductDetails.js
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const MoreProductDetails = () => {
+  const { id } = useParams();
+  const [products, setProduct] = useState([]);
+  // const [servicesDetails,setServicesDetails] = useState([]);
 
-     const { id } = useParams();
-     const [products, setProduct] = useState([]);
-     // const [servicesDetails,setServicesDetails] = useState([]);
-   
-     useEffect(() => {
-       fetch(`/product.json`)
-         .then((res) => res.json())
-         .then((data) => setProduct(data));
-     }, []);
-     const details = products.find((s) => s.id === id);
-     return (
-          <div className='py-5 App'>
-               <div className=" mt-5 my-5">
-       
+  useEffect(() => {
+    fetch(`/product.json`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+  const details = products.find((s) => s.id === id);
+  return (
+    <div className="py-5   ">
+      <div className=" mt-5          d-flex    my-5">
+        <div className="col-12 col-sm-12 col-lg-6">
+          <img src={details?.img} className="img-fluid p-4 w-100" alt="" />
+        </div>
 
-       <h3>Name:{details?.name}</h3>
-       <img src={details?.img} alt="" />
-       {/* <h6>Price:${details?.price}</h6> */}
-       <h6>Price:${details?.description}</h6>
-       <h4>If you are agree to checkout</h4>
-       {/* <Link to="/checkout">
-         <button className="btn btn-info"> Please Checkout</button>
-       </Link> */}
-     </div>
-          </div>
-     );
+        <div className="col-12 col-sm-12 col-lg-6 mt-5">
+        
+          <h3>Name:{details?.name}</h3>
+          <h6>Description:{details?.description}</h6>
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MoreProductDetails;
